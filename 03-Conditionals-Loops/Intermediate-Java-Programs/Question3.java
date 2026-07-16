e8e4ce8b07b292dde02fc704490a687920cbd1b3
Aditya choubey <adityachoubey987@gmail.com>
2026-07-16 21:33:37 +0530

    day 12 done

--- 03-Conditionals-Loops/Intermediate-Java-Programs/Question3.java
+++ 03-Conditionals-Loops/Intermediate-Java-Programs/Question3.java
@@ -3,8 +3,19 @@
  *
  * Calculate Average Of N Numbers
  */
+import java.util.Scanner;
 public class Question3 {
     public static void main(String[] args) {
         // TODO: implement solution
+        try(Scanner sc = new Scanner(System.in)) {
+            System.out.println("enter the numbers");
+            int n=3;
+            int a= sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+                int sum=a+b+c;
+                double average = sum/n;
+            System.out.println(average);
+        }
     }
 }
