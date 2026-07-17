2b5dc8647a68d7d5a05a3608a40203ab8c2f2b0a
Aditya choubey <adityachoubey987@gmail.com>
2026-07-17 16:52:49 +0530

    Day 13 lcm hcf gcd

--- 03-Conditionals-Loops/Intermediate-Java-Programs/gcd.java
+++ 03-Conditionals-Loops/Intermediate-Java-Programs/gcd.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
 public class gcd {
+static int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+static long lcm(int a, int b) {
+    return (long) a * b / gcd(a, b);     // reuse gcd!
+}
+
     public static void main(String args[]) {
         try(Scanner sc= new Scanner(System.in)) {
             System.out.println("enter the numbers");
             int num = sc.nextInt();
             int num1 = sc.nextInt();
-            int a=num,b=num1;
-            while(a!=0&&b!=0) {
-                int temp=a;
-                a=a%b;
-                b=temp;
-                
-            }
-            System.out.println(a);
+            System.out.println(gcd(num,num1));
+            System.out.println(lcm(num, num1));
         }
     }
 
