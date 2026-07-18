44a7949563fac34f2e01ab2e4085874097710b71
Aditya choubey <adityachoubey987@gmail.com>
2026-07-18 15:19:04 +0530

    Day 14: Perfect Number

--- 03-Conditionals-Loops/Intermediate-Java-Programs/Question22.java
+++ 03-Conditionals-Loops/Intermediate-Java-Programs/Question22.java
@@ -3,8 +3,28 @@
  *
  * Perfect Number In Java
  */
+import java.util.Scanner;
 public class Question22 {
+    static void perfectNumber (int a){
+        int temp = a;
+        int sum =0;
+        for(int i=1;i<a;i++) {
+            if(temp%i==0) {
+                sum=sum+i;
+            }
+        }
+        if(sum==a) {
+            System.out.println("yes it is");
+        }else{
+            System.out.println("no");
+        }
+    }
     public static void main(String[] args) {
         // TODO: implement solution
+        System.out.println("Enter the number");
+        try(Scanner sc = new Scanner(System.in)) {
+            int num = sc.nextInt();
+            perfectNumber(num);
+        }
     }
 }
