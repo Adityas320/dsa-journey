3a43a471d08433677a595f56f76b521f468a6fdc
Aditya choubey <adityachoubey987@gmail.com>
2026-07-22 16:23:00 +0530

    Day-18: Max/Min of three, primes in rage(root n + gieard),Leap year

--- 04-Functions/Question1.java
+++ 04-Functions/Question1.java
@@ -5,8 +5,54 @@
  *
  * Link: https://www.java67.com/2019/05/how-to-find-largest-and-smallest-of-three-numbers-in-java.html
  */
+import java.util.Scanner;
 public class Question1 {
+    static int maximum1(int a,int b,int c){
+            int max = a;
+             if (b > max) max = b;
+                if (c > max) max = c;
+                return max;
+
+    }
+        static int minimum2(int a,int b,int c){
+            int min = a;
+             if (b < min) min = b;
+                if (c < min) min = c;
+                return min;
+
+    }
+    static int maximum(int a, int b, int c) {
+        if(a>b) {
+            if(a>c) {
+                return a;
+            } else {
+                return c;
+            }
+        }if(b>c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+    static int minimum(int a,int b,int c) {
+        if(a<b) {
+            if(a<c) {
+                return a;
+            } else {
+                return c;
+            }
+        }
+        if(b<c) {
+            return b;
+        }else {
+            return c;
+        }
+    }
     public static void main(String[] args) {
         // TODO: implement solution
+        try(Scanner sc = new Scanner(System.in)) {
+            System.out.println(maximum1(12, 13, 14));
+            System.out.println(minimum2(12, 13, 14));
+        }
     }
 }
--- 04-Functions/Question13.java
+++ 04-Functions/Question13.java
@@ -4,9 +4,30 @@
  * Write a function that returns all prime numbers between two given numbers.
  *
  * Link: https://www.geeksforgeeks.org/program-to-find-prime-numbers-between-given-interval/
+ * 
  */
+import java.util.Scanner;
 public class Question13 {
+    static boolean primeNumber(int a) {
+        for(int i=2;i*i<=a;i++) {
+            if(a%i==0) {
+            return  false;
+            }
+        }
+        return  true;
+    }
     public static void main(String[] args) {
         // TODO: implement solution
+        try(Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the range of number");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            for(int i =a;i<=b;i++) {
+                if(primeNumber(i)==true) {
+                    System.out.println(i);
+                }
+            }
+
+        }
     }
 }
