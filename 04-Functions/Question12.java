68aa4198aa35a520de54d00bbf899cceebe2f892
Aditya choubey <adityachoubey987@gmail.com>
2026-07-24 22:02:09 +0530

    Day 20: Pythagorean triplet + Sum of first n naturals

--- 04-Functions/Question12.java
+++ 04-Functions/Question12.java
@@ -5,8 +5,30 @@
  *
  * Link: https://www.geeksforgeeks.org/find-pythagorean-triplet-in-an-unsorted-array/
  */
+import java.util.Scanner;
 public class Question12 {
+    static void pythaTriplet(int a,int b,int c) {
+       int first= square(a);
+       int second=square(b);
+       int third = square(c);
+       if(first + second== third || second + third == first || third+first==second) {
+        System.out.println("yes they are triplet");
+
+       }else {
+        System.out.println("not a triplet");
+       }
+    }
+    static int square(int n) {
+        return n*n;
+    }
     public static void main(String[] args) {
         // TODO: implement solution
+        try(Scanner sc = new Scanner(System.in)) {
+            int a = sc.nextInt();
+            int b= sc.nextInt();
+            int c= sc.nextInt();
+            pythaTriplet(a, b, c);
+            
+        }
     }
 }
--- 04-Functions/Question14.java
+++ 04-Functions/Question14.java
@@ -5,8 +5,15 @@
  *
  * Link: https://www.geeksforgeeks.org/program-find-sum-first-n-natural-numbers/
  */
+import java.util.Scanner;
 public class Question14 {
     public static void main(String[] args) {
         // TODO: implement solution
+        try(Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int sum = (n*(n+1))/2;
+            System.out.println("sum of "+ n +" natural numbers "+ sum);
+        }
+
     }
 }
