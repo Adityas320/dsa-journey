44f6a8e4277d17b719530450862f8a89ba826b0f
Aditya choubey <adityachoubey987@gmail.com>
2026-07-25 21:24:30 +0530

    day21: NcR and NpR (reuses factorial)

--- 03-Conditionals-Loops/Intermediate-Java-Programs/Question15.java
+++ 03-Conditionals-Loops/Intermediate-Java-Programs/Question15.java
@@ -3,8 +3,32 @@
  *
  * Find Ncr & Npr
  */
+import java.util.Scanner;
 public class Question15 {
+    static long factorial(int n) {
+        long factorial = 1;
+        for(int i=1; i<=n;i++) {
+            factorial=factorial*i;
+        }
+        return factorial;
+    }
+    static long ncr (int n,int r) {
+        return factorial(n)/(factorial(r)*factorial(n-r));
+    }
+    static long npr (int n, int r) {
+        return factorial(n)/factorial(n-r);
+    }
     public static void main(String[] args) {
         // TODO: implement solution
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.println("enter both numbers");
+            int n = sc.nextInt();
+            int r= sc.nextInt();
+            if(r>=0&&r<=n) {
+            System.out.println("the ncr "+ncr(n, r)+ " and npr is "+npr(n, r));
+            } else {
+                System.out.println("not compitable");
+            }
+        }
     }
 }
