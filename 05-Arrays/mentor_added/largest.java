cbcd2419187db996f52d73c79c2fb92efd74ae97
Aditya choubey <adityachoubey987@gmail.com>
2026-07-27 21:15:14 +0530

    day 22 arrays started with largest and addition

---
+++ 05-Arrays/mentor_added/largest.java
@@ -0,0 +1,17 @@
+
+public class largest {
+    public static int largest (int arr[]) {
+            int largest = arr[0];
+        for(int i=1;i<arr.length;i++) {
+            if(arr[i]>largest) {
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        System.out.println(largest(arr));
+    }
+}
