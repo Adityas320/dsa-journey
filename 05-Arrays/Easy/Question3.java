411ee9de924fd87c28e837add955d51b3195947c
Aditya choubey <adityachoubey987@gmail.com>
2026-07-30 13:30:34 +0530

    Day 25: highest altitude single-pass rewrite +smaller numbers than current #5

--- 05-Arrays/Easy/Question3.java
+++ 05-Arrays/Easy/Question3.java
@@ -6,7 +6,29 @@
  * Link: https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
  */
 public class Question3 {
+    public static boolean[] ListofCandies(int a,int arr[]) {
+        boolean[] brr= new boolean[arr.length];
+        int max=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max) {
+                max=arr[i];
+            }
+        }
+        for(int i=0;i<arr.length;i++) {
+            if(arr[i]+a>=max) {
+                brr[i]=true;
+            }
+        }
+        return brr;
+    }
     public static void main(String[] args) {
         // TODO: implement solution
+        int arr[]={2,3,5,1,3};
+        int extra =4;
+       boolean brr[]= ListofCandies(extra, arr);
+       for(int i=0;i<brr.length;i++) {
+        System.out.println(brr[i]);
+       }
+        
     }
 }
--- 05-Arrays/Easy/Question5.java
+++ 05-Arrays/Easy/Question5.java
@@ -5,8 +5,27 @@
  *
  * Link: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
  */
+// n2 we want n we will try 
 public class Question5 {
+    public static int[] smaller(int arr[]) {
+        int counting[]= new int[arr.length];
+        for(int i=0;i<arr.length;i++) {
+            int count =0;
+            for(int j=0;j<arr.length;j++) {
+                if(arr[i]>arr[j]) {
+                    count++;
+                }
+            }
+            counting[i]=count;
+        }
+        return counting;
+    }
     public static void main(String[] args) {
         // TODO: implement solution
+        int arr []={8,1,2,2,3};
+       int brr[]= smaller(arr);
+        for(int i=0;i<brr.length;i++) {
+            System.out.println(brr[i]);
+        }
     }
 }
